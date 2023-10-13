240dc11291b9e0fe2a62797fb2c94ab05a851356
eugenia0804 <67468941+eugenia0804@users.noreply.github.com>
2023-10-12 20:27:35 -0500

    simplified the code

--- improved.cpp
+++ improved.cpp
@@ -1,68 +1,46 @@
 #include <iostream>
 #include <vector>
 #include <chrono>
+#include <bitset>
 
-std::vector<int> sieveOfEratosthenes(int n) {
-    std::vector<bool> isPrime(n + 1, true);
-    std::vector<int> primes;
-    
-    isPrime[0] = isPrime[1] = false;
-    
-    for (int p = 2; p * p <= n; ++p) {
+const int MAX = 2500;
+
+std::bitset<MAX + 1> isPrime; 
+
+void findPrime() {
+    isPrime.set();  
+    isPrime[0] = isPrime[1] = 0;
+
+    for (int p = 2; p * p <= MAX; ++p) {
         if (isPrime[p]) {
-            for (int i = p * p; i <= n; i += p) {
-                isPrime[i] = false;
+            for (int i = p * p; i <= MAX; i += p) {
+                isPrime[i] = 0;
             }
         }
     }
-    
-    for (int p = 2; p <= n; ++p) {
-        if (isPrime[p]) {
-            primes.push_back(p);
-        }
-    }
-    
-    return primes;
-}
-
-std::vector<int> primeFactors(int n, const std::vector<int>& primes) {
-    std::vector<int> uniquePrimes;
-
-    for (int prime : primes) {
-        while (n % prime == 0) {
-            uniquePrimes.push_back(prime);
-            n /= prime;
-        }
-    }
-
-    if (n > 1) {
-        uniquePrimes.push_back(n);
-    }
-
-    return uniquePrimes;
 }
 
 int main() {
-    const int MAX = 2500;
-
-    auto start = std::chrono::high_resolution_clock::now(); // Start the timer
-    std::vector<int> primes = sieveOfEratosthenes(MAX);
-    auto end = std::chrono::high_resolution_clock::now(); // End the timer
+    auto start = std::chrono::high_resolution_clock::now(); 
+    findPrime();
+    auto end = std::chrono::high_resolution_clock::now(); 
     std::chrono::duration<double> duration = end - start;
-    
+
     std::cout << "Prime number generation took " << duration.count() << " seconds." << std::endl;
 
-    start = std::chrono::high_resolution_clock::now(); // Start the timer
+    start = std::chrono::high_resolution_clock::now(); 
+
+    std::vector<int> uniquePrimes;
+
     for (int i = 2; i <= MAX; i++) {
-        std::vector<int> factors = primeFactors(i, primes);
-        
-        if (factors.size() == 1 && factors[0] == i) {
-            std::cout << i << " is prime." << std::endl;
+        if (isPrime[i]) {
+            uniquePrimes.push_back(i);
         }
     }
-    end = std::chrono::high_resolution_clock::now(); // End the timer
+
+    end = std::chrono::high_resolution_clock::now(); 
     duration = end - start;
-    
+
     std::cout << "Prime factorization took " << duration.count() << " seconds." << std::endl;
 
     return 0;
